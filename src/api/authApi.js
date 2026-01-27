f1cb7fbbe631d8ebb086137ec21f8047db789b65
sauravganguly7678 <saurav.ganguly@in.ey.com>
2026-01-27 11:00:44 +0530

    login post api endpoint changed

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -2,7 +2,7 @@ import { authApiClient } from "./clients";
 import AuthService from "../auth/AuthService";
 
 export const login = async (username, password) => {
-  const response = await authApiClient.post("/v1/auth/login", {
+  const response = await authApiClient.post("/api/v1/auth/login", {
     username,
     password,
   });
